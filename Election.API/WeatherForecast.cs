4de454b8b23b49189fbaf30298979a8910b776c1
Jean Carlos Soto <jeanc.soto020@gmail.com>
2022-03-24 11:35:11 -0400

    dsfsdfs

--- Election.API/WeatherForecast.cs
+++ Election.API/WeatherForecast.cs
@@ -9,5 +9,7 @@ namespace Election.API
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string? Summary2 { get; set; }
+        public string? Summary3 { get; set; }
     }
 }
